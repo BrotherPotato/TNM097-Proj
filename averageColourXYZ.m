4b8e89f1d303ce2b1b772b945ea24986775934c5
Max Wiklundh <wiklundhmax@gmail.com>
2025-03-04 18:22:10 +0100

    stuff 2

--- averageColourXYZ.m
+++ averageColourXYZ.m
@@ -1,18 +1,22 @@
 function colourXYZ = averageColourXYZ(imageRGB)
 
-load("xyz.mat");
-load("illum.mat");
-load("DLP.mat");
+%load("xyz.mat");
+%load("illum.mat");
+%load("DLP.mat");
+
+load("M_XYZ2RGB.mat")
 
 meanRGB(1) = mean(mean(imageRGB(:,:,1)));
 meanRGB(2) = mean(mean(imageRGB(:,:,2)));
 meanRGB(3) = mean(mean(imageRGB(:,:,3)));
 
-Srgb = DLP * meanRGB';
+%Srgb = DLP * meanRGB';
 
-k = 100 ./ sum(xyz(:,2)' .* CIED65);
+%k = 100 ./ sum(xyz(:,2)' .* CIED65);
 
-colourXYZ = (k * xyz' * Srgb);
+%colourXYZ = (k * xyz' * Srgb);
 
+
+colourXYZ = inv(M_XYZ2RGB) * meanRGB';
 
 end
