1e5f972cb48d56029f86bc3c791dd9d5faa8f69e
Magnus <91549385+KoksILasten@users.noreply.github.com>
2025-03-05 19:48:20 +0100

    subsamplar bilder men gör inget med den subsamplade bilden

--- subsampleImage.m
+++ subsampleImage.m
@@ -1,8 +1,36 @@
-function [outputArg1,outputArg2] = subsampleImage(imgtile, length)
+function [outputArg1] = subsampleImage(targetTile, samplesPerTile)
+sPT = sqrt(samplesPerTile); %Inte supereffektivt men den är mer intiutivt me tinks
 
-%SUBSAMPLEIMAGE Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+[rows, cols, ~] = size(targetTile);
+
+% Calculate the size of each axis part
+partRows = floor(rows / sPT);
+partCols = floor(cols / sPT);
+
+averages = zeros(sPT, sPT);
+
+% Loop through each part and compute the average
+for i = 1:sPT
+    for j = 1:sPT
+
+        % Define the region of interest (ROI)
+        rowStart = (i-1) * partRows + 1;
+        rowEnd = i * partRows;
+        colStart = (j-1) * partCols + 1;
+        colEnd = j * partCols;
+        
+        % Extract the smaller part
+        smallPart = targetTile(rowStart:rowEnd, colStart:colEnd, :);
+        
+        % Compute the average of the smaller part
+        averages(i, j) = mean(mean(smallPart(:)));
+    end
+end
+
+% Display the averages
+disp('Averages of each part:');
+disp(averages);
+
+
 end
 
