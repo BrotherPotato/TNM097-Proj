eaa04aa83a3ccfe305c25014670f876ceb9a52f2
Max Wiklundh <wiklundhmax@gmail.com>
2025-03-12 01:11:10 +0100

    stuff

--- preprocessImages2.m
+++ preprocessImages2.m
@@ -1,51 +1,39 @@
-function outputArray = preprocessImages2(imgArray, threshold, tileRatio, nrOfPrimaryColours)
+function outputArray = preprocessImages2(imgArray, threshold, tileRatio)
 
 % Initialize waitbar
 h = waitbar(0, 'Preprocessing Images...');
-
 % Initialize output array
 outputArray = {};
-    
+length(imgArray)    
 %add first image to output array
-outputArray{end+1} = imresize(imgArray{1}, tileRatio);
-colours = getImportantColours(imresize(imgArray{1}, tileRatio), nrOfPrimaryColours);
-[~, cMax] = size(colours);
-for c = 1:cMax
-    outputArray{end, c+1} = colours(:,c);
-end
-outputArray
+outputArray{end+1, 1} = imresize(imgArray{1}, tileRatio);
+outputArray{end, 2} = imgArray{1, 2};
+%addedIndex(1) = 0;
+
 % Loop through each image in the array
 for i = 1:length(imgArray)
-    currentImage = imresize(imgArray{i}, tileRatio);
-    currentImageXYZ = averageColourXYZ(currentImage);
-    % Update waitbar
-    waitbar(i/length(imgArray), h);
-    imageMatch = false;
-    [numOfImages, ~] = size(outputArray);
-    for j = 1:numOfImages
-        
-        compareImage = imresize(outputArray{j,1}, tileRatio);
-        compareImageXYZ = averageColourXYZ(compareImage);
+        % Update waitbar
+        waitbar(i/length(imgArray), h);
 
-        % use calcDeltaE
-        [meanDeltaE, ~] = calcDeltaE(currentImageXYZ', compareImageXYZ');
-
-        %add image to output array if above treshold
-        if (meanDeltaE < threshold)
-            imageMatch = true;
-            break;
-        end 
-    end
-
-    if imageMatch == false
-        outputArray{end+1,1} = currentImage;
-        colours = getImportantColours(currentImage, nrOfPrimaryColours);
-        [~, cMax] = size(colours);
-        for c = 1:cMax
-            outputArray{end, c+1} = colours(:,c);
+        targetPrimaryColours = imgArray{i,2};
+        shouldAdd = true;
+        [a, ~] = size(outputArray);
+        for ii = 1:a
+            currentPrimaryColours = outputArray{ii,2};
+            % if they match we dont want it
+            if comparePrimaryColours(targetPrimaryColours, currentPrimaryColours, threshold) == true
+                shouldAdd = false;
+                break
+            end
         end
-    end
+
+        if shouldAdd == true
+            outputArray{end+1, 1} = imresize(imgArray{i}, tileRatio);
+            outputArray{end, 2} = imgArray{i, 2};
+        end
+
 
 end
-close(h);
+
+close(h)
 end
